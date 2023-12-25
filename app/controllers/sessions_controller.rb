aa5b0b8354781370a84c700dcdbd1a3735c3e197
Ashwin M <thatsashwin@gmail.com>
2023-12-25 22:42:57 +0530

    minor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
   def create
     @user = User.from_omniauth(request.env["omniauth.auth"])
 

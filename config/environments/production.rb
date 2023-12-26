d43eb3da64df65d4d9d2e85df340bbed86c1ba15
Ashwin M <thatsashwin@gmail.com>
2023-12-26 17:36:14 +0530

    config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,10 +46,10 @@ Rails.application.configure do
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
-  # config.assume_ssl = true
+  config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = false
+  config.force_ssl = true
 
   config.active_record.sqlite3_production_warning = false
 
@@ -90,10 +90,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = [
-    "kamal.directory",     # Allow requests from example.com
-    /.*\.kamal\.directory/ # Allow requests from subdomains like `www.example.com`
-  ]
+  # config.hosts = [
+  #   "kamal.directory",     # Allow requests from example.com
+  #   /.*\.kamal\.directory/ # Allow requests from subdomains like `www.example.com`
+  # ]
   # Skip DNS rebinding protection for the default health check endpoint.
-  config.host_authorization = {exclude: ->(request) { request.path == "/up" }}
+  # config.host_authorization = {exclude: ->(request) { request.path == "/up" }}
 end

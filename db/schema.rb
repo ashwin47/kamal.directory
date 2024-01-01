634cd6ccb3824578f3942c515b17eb348efefeed
Ashwin M <thatsashwin@gmail.com>
2024-01-01 15:44:49 +0530

    Add additional files (#4)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_24_182233) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_31_152858) do
   create_table "app_gems", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -62,6 +62,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_24_182233) do
     t.datetime "updated_at", null: false
     t.integer "server_count"
     t.integer "accessory_count"
+    t.string "title"
+    t.text "dockerfile"
+    t.text "env"
+    t.index ["user_id"], name: "index_kamalfiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
